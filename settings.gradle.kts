ae60017fd6de533b8dd33bab39ef24a1a9dc0c20
Djaytan <26904516+Djaytan@users.noreply.github.com>
2023-04-10 20:54:31 +0200

    initialize project with Spring Initializr

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "spring-boot-dummy-airline-app"
---
+++ src/main/kotlin/com/djaytan/dummy/springairline/SpringBootDummyAirlineAppApplication.kt
@@ -0,0 +1,11 @@
+package com.djaytan.dummy.springairline
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootDummyAirlineAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootDummyAirlineAppApplication>(*args)
+}
